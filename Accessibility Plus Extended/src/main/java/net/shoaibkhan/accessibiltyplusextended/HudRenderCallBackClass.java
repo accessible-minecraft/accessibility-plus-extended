742302480d607e21ca40d9983ffcdcd9d3c2d156
Shoaib Khan <mdshoaibkhan.1999@gmail.com>
2021-04-07 01:04:26 +0530

    Experimenting

--- Accessibility Plus Extended/src/main/java/net/shoaibkhan/accessibiltyplusextended/HudRenderCallBackClass.java
+++ Accessibility Plus Extended/src/main/java/net/shoaibkhan/accessibiltyplusextended/HudRenderCallBackClass.java
@@ -4,22 +4,34 @@ import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.command.argument.EntityAnchorArgumentType;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
-
-import java.util.Locale;
+import net.minecraft.util.math.Vec3d;
+import org.lwjgl.glfw.GLFW;
 
 public class HudRenderCallBackClass {
     private MinecraftClient client;
-    private String tempBlock, tempBlockPos;
+    private String tempBlock="", tempBlockPos="";
     private String tempEntity="",tempEntityPos="";
-    public  HudRenderCallBackClass(){
+    public  HudRenderCallBackClass(KeyBinding num_5){
         client = MinecraftClient.getInstance();
         HudRenderCallback.EVENT.register((__,___) -> {
-            if (!client.isPaused())crosshairTarget();
+            try {
+                if (!client.isPaused()) crosshairTarget();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            while (num_5.wasPressed()) {
+                BlockPos pos =  client.player.getBlockPos();
+                Vec3d vec3d = new Vec3d(pos.getX()+0, pos.getY()+1, pos.getZ()+0);
+                client.player.lookAt(EntityAnchorArgumentType.EntityAnchor.EYES,vec3d);
+            }
         });
     }
 
@@ -47,17 +59,39 @@ public class HudRenderCallBackClass {
                 }
                 break;
             case ENTITY:
-                EntityHitResult entityHitResult = (EntityHitResult) hit;
-                if(!(((EntityHitResult) hit).getEntity().getType()+"").equalsIgnoreCase(tempEntity) || !(((EntityHitResult) hit).getEntity().getBlockPos()+"").equalsIgnoreCase(tempEntityPos)) {
-                    tempEntity = ((EntityHitResult) hit).getEntity().getType()+"";
-                    tempEntityPos = ((EntityHitResult) hit).getEntity().getBlockPos()+"";
-                    tempBlockPos = "";
-                    tempBlock = "";
-                    text = entityHitResult.getEntity().getType() + "";
-                    text = text.substring(text.lastIndexOf('.') + 1);
-                    if (text.contains("_")) text = text.replace("_", " ");
-                    System.out.println(text);
-                    narrate(text);
+                try{
+                    EntityHitResult entityHitResult = (EntityHitResult) hit;
+                    if (!(((EntityHitResult) hit).getEntity().getType() + "").equalsIgnoreCase(tempEntity) || !(((EntityHitResult) hit).getEntity().getBlockPos() + "").equalsIgnoreCase(tempEntityPos)) {
+                        tempEntity = ((EntityHitResult) hit).getEntity().getType() + "";
+                        tempEntityPos = ((EntityHitResult) hit).getEntity().getBlockPos() + "";
+                        tempBlockPos = "";
+                        tempBlock = "";
+                        text = entityHitResult.getEntity().getType() + "";
+                        text = text.substring(text.lastIndexOf('.') + 1);
+                        if (text.contains("_")) text = text.replace("_", " ");
+                        System.out.println(text);
+                        narrate(text);
+                    }
+                } catch (Exception e) {
+                    try{
+                        BlockHitResult blockHitResult1 = (BlockHitResult) hit;
+                        BlockState blockState1 = client.world.getBlockState(blockHitResult1.getBlockPos());
+                        Block block1 = blockState1.getBlock();
+                        if ((!tempBlock.equalsIgnoreCase(block1 + "") || !(tempBlockPos.equalsIgnoreCase(blockHitResult1.getBlockPos() + ""))) && !(blockState1 + "").toLowerCase().contains("sign")) {
+                            tempBlock = block1 + "";
+                            tempBlockPos = blockHitResult1.getBlockPos() + "";
+                            tempEntityPos = "";
+                            tempEntity = "";
+                            String side = blockHitResult1.getSide().asString();
+                            String name = block1.getTranslationKey();
+                            name = name.substring(name.lastIndexOf('.') + 1);
+                            if (name.contains("_")) name = name.replace("_", " ");
+                            text = name + ", " + side + " face";
+                            narrate(text);
+                        }
+                    } catch (Exception e1){
+                        System.out.println(e1);
+                    }
                 }
                 break;
         }
--- Accessibility Plus Extended/src/main/java/net/shoaibkhan/accessibiltyplusextended/modInit.java
+++ Accessibility Plus Extended/src/main/java/net/shoaibkhan/accessibiltyplusextended/modInit.java
@@ -1,12 +1,23 @@
 package net.shoaibkhan.accessibiltyplusextended;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class modInit implements ModInitializer {
 	HudRenderCallBackClass hudRenderCallBackClass;
+	KeyBinding num_5;
 	@Override
 	public void onInitialize() {
 		System.out.println("Hello Fabric world!");
-		hudRenderCallBackClass = new HudRenderCallBackClass();
+		num_5 = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"Center Camera",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_5,
+				"Accessibility Plus Extended"
+		));
+		hudRenderCallBackClass = new HudRenderCallBackClass(num_5);
 	}
 }
